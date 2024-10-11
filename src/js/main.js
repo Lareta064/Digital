a6658ef3b453743302d0ba5462ba0e95f1a931dc
Lareta064 <lareta064@gmail.com>
2024-10-11 19:21:15 +0300

    service-slider

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,48 @@ document.addEventListener("DOMContentLoaded", function () {
 		else{
 			pageHeader.classList.remove('header-fixed');
 		}
-	})
+	});
+
+	let serviceSlider;
+
+	function initSwiper() {
+		const screenWidth = window.innerWidth;
+
+		if (screenWidth >= 599 && !serviceSlider) {
+			// Инициализация Swiper на экранах больше или равных 768px
+			serviceSlider = new Swiper('.cards-swiper', {
+				slidesPerView: 'auto',
+				spaceBetween: 15,
+				speed: 1500,
+				loop:true,
+				autoplay:{
+					delay: 1000,
+					disableOnInteraction: false,
+				},
+				responsive:{
+					1024:{
+						spaceBetween: 25,
+					},
+					1199:{
+						spaceBetween: 40,
+					}
+				}
+			});
+		} else if (screenWidth < 600 && serviceSlider) {
+			// Уничтожаем Swiper на экранах меньше 768px
+			serviceSlider.destroy(true, true);
+			serviceSlider = undefined; // Сбрасываем объект слайдера
+			// Убираем стили Swiper, чтобы слайды выстроились в колонку
+			const slides = document.querySelectorAll('.swiper-slide');
+			slides.forEach(slide => {
+				slide.style.width = '100%';
+				
+			});
+		}
+	}
+
+	// Вызываем функцию при загрузке страницы и при изменении размера окна
+	window.addEventListener('load', initSwiper);
+	window.addEventListener('resize', initSwiper);
 
 });
